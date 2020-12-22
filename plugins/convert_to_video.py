713d5942649b886985797b2bc5115b731f1a0d0f
indraabhushan.pr <68438304+indru-dudez@users.noreply.github.com>
2020-12-22 17:17:29 +0530

    Update convert_to_video.py

--- plugins/convert_to_video.py
+++ plugins/convert_to_video.py
@@ -32,9 +32,9 @@ from hachoir.parser import createParser
 from PIL import Image
 
 
-@pyrogram.Client.on_message(pyrogram.Filters.command(["convert2video"]))
+@pyrogram.Client.on_message(pyrogram.Filters.command(["convert2video@SINNER_MX_bot"]))
 def convert_to_video(bot, update):
-    TRChatBase(update.from_user.id, update.text, "convert2video")
+    TRChatBase(update.from_user.id, update.text, "convert2video@SINNER_MX_bot")
     if str(update.from_user.id) not in Config.SUPER_DLBOT_USERS:
         bot.send_message(
             chat_id=update.chat.id,
