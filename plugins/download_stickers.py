1262590e7bd83f474c9d8ab60fcb63e08774443a
indraabhushan.pr <68438304+indru-dudez@users.noreply.github.com>
2020-12-22 22:37:51 -0800

    Update download_stickers.py

--- plugins/download_stickers.py
+++ plugins/download_stickers.py
@@ -27,7 +27,7 @@ from helper_funcs.chat_base import TRChatBase
 from helper_funcs.display_progress import progress_for_pyrogram
 
 
-@pyrogram.Client.on_message(pyrogram.Filters.sticker)
+@pyrogram.Client.on_message(pyrogram.Filters.command(["getsticker"]))
 def DownloadStickersBot(bot, update):
     TRChatBase(update.from_user.id, update.text, "getSticker")
     if str(update.from_user.id) in Config.BANNED_USERS:
